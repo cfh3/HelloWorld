3ee171cd4fb46731ea976266a212d6b8465dab74
Chip Hofmann <chofmann@anonyome.com>
2016-05-08 09:54:06 -0600

    minor change #1

--- HelloWorld.py
+++ HelloWorld.py
@@ -4,7 +4,7 @@ def MyClass():
     print "In MyClass"
 
 def main():
-    print "In main"
+    print "In main, that was run fro the instantiated class"
 
 if __name__ == "__main__":
     app = MyClass()
