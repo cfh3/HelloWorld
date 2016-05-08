2af07c902284f7a0f82141aaabf15077cb751d6c
Chip Hofmann <chofmann@anonyome.com>
2016-05-08 10:13:06 -0600

    fixed a typo

--- HelloWorld.py
+++ HelloWorld.py
@@ -4,7 +4,7 @@ def MyClass():
     print "In MyClass"
 
 def main():
-    print "In main, that was run fro the instantiated class"
+    print "In main, that was run from the instantiated class"
 
 if __name__ == "__main__":
     app = MyClass()
